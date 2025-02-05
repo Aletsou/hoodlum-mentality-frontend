b91b29c7d8cc6898932ab3798ede051f27eeadc2
alexandrostsoulakidis <al.tsoulakidis@gmail.com>
2025-02-05 12:07:56 +0100

    createRoot

--- src/index.js
+++ src/index.js
@@ -6,10 +6,11 @@ import store from './redux/store';
 import App from './App';
 import './styles/App.css';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(
   <Provider store={store}>
     <App />
-  </Provider>,
-  document.getElementById('root')
+  </Provider>
 );
 
